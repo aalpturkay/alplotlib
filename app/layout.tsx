bfcb46293f963f0ed07a4bf73e0ce478c92bcb6f
Alp Turkay <aykutalpturkay@gmail.com>
2024-12-16 11:00:25 +0300

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./components/footer";
 import { baseUrl } from "./sitemap";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -62,6 +63,7 @@ export default function RootLayout({
           <Analytics />
           <SpeedInsights />
         </main>
+        <GoogleAnalytics gaId="GTM-58PSGTSX" />
       </body>
     </html>
   );
